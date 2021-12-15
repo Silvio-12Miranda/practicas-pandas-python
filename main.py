bfa76703c5fe3cf69465299e7b88903296199fa8
Silvio Miranda <silviom727@gmail.com>
2021-12-15 14:52:27 -0600

    lectura de ocumentos csv

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
 def run():
-    data = {
-            'nombres' : ['Silvio', 'Joserra', 'Miguel'],
-            'edades' : [26, 26, 29],
-            'altura' : [1.75, 1.75, 1.70],
-            'peso' : [85, 110, 100]
-            }
-    x = pd.DataFrame(data)
+    x = pd.read_csv('data.csv')
     print(x)
 
 if __name__ == '__main__':
