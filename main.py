0db0660cfd151ba1bd1d644e616a4d4eaa4445f6
Silvio Miranda <silviom727@gmail.com>
2021-12-17 02:47:59 -0600

    uso de graficos

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 def run():
     x = pd.read_csv('datacorrelacion.csv')
-    x = x.corr() 
-
-    print(x)
+    x.plot(kind = 'scatter', y = 'Maxpulse', x = 'Calories')
+    plt.show()
 
 if __name__ == '__main__':
     run()
