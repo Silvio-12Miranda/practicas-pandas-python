425e22bd1bea13e678dfec33e5db9d4dd8ddb1fc
Silvio Miranda <silviom727@gmail.com>
2021-12-17 17:53:36 -0600

    first commit matplotlib

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 def run():
     x = pd.read_csv('datacorrelacion.csv')
-    x.plot(kind = 'scatter', y = 'Maxpulse', x = 'Calories')
+    x.plot(kind = 'scatter', y = 'Duration', x = 'Calories')
     plt.show()
 
 if __name__ == '__main__':
