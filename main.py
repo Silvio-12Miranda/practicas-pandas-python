a4940cf39d83e7ca77cf028fc64c5b91c269dc81
Silvio Miranda <silviom727@gmail.com>
2021-12-16 21:33:26 -0600

    trabajo de correlacion

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import pandas as pd
 
 def run():
-    x = pd.read_csv('dirtydata.csv')
-    x.dropna(inplace = True)
-    #x.fillna(1, inplace = True)
-    #x['Calories'].fillna('VALUE', inplace = True)
-    x['Date'] = pd.to_datetime(x['Date'])
-    x.dropna(subset = ['Date'], inplace = True)
-    x.loc[7, 'Duration'] = 45
-
-    for i in x.index:
-        if x.loc[i, 'Duration'] > 120:
-            x.loc[i, 'Duration'] = 120
-    for i in x.index:
-        if x.loc[i, 'Duration'] < 60:
-            x.drop(i, inplace = True)
-
-    #x = x.duplicated()
-    x.drop_duplicates(inplace = True)
+    x = pd.read_csv('datacorrelacion.csv')
+    x = x.corr() 
 
     print(x)
 
