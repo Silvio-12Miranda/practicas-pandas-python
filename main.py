947393da897d0a007dab55e06da5a36bd7ee3a17
Silvio Miranda <silviom727@gmail.com>
2021-12-16 21:01:22 -0600

    limpieza de datos

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
 def run():
-    #pd.options.display.max_row = 1000
-    x = pd.read_json('datajs.js')
+    x = pd.read_csv('dirtydata.csv')
+    #x.dropna(inplace = True)
+    #x.fillna(1, inplace = True)
+    #x['Calories'].fillna('VALUE', inplace = True)
+    x['Date'] = pd.to_datetime(x['Date'])
+    x.dropna(subset = ['Date'], inplace = True)
+    x.loc[7, 'Duration'] = 45
+
+    for i in x.index:
+        if x.loc[i, 'Duration'] > 120:
+            x.loc[i, 'Duration'] = 120
+    for i in x.index:
+        if x.loc[i, 'Duration'] < 60:
+            x.drop(i, inplace = True)
+
     print(x)
 
 if __name__ == '__main__':
